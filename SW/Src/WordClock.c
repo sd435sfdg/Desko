07a2aef9311fa0870300b1b5fef7643130f961dc
forcehero <36795737+forcehero@users.noreply.github.com>
2018-04-16 16:22:57 +0700

    Chỉnh sửa hàng lối

--- SW/Src/WordClock.c
+++ SW/Src/WordClock.c
@@ -3,8 +3,7 @@
 void send_packet(SPI_HandleTypeDef *hspi, uint8_t *p_data)
 {
 	HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_RESET);
-	for(int loop = 0;loop < NUM_MAX7219; loop++)
-	{
+	for(int loop = 0;loop < NUM_MAX7219; loop++){
   	HAL_SPI_Transmit(hspi, p_data + loop*sizeof(uint16_t), 1, 100);
 	}
 	HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_SET);
@@ -14,8 +13,7 @@ void send_all(SPI_HandleTypeDef *hspi, uint16_t data)
 {
 	uint16_t *p_data = &data;
 	HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_RESET);
-	for(int loop = 0;loop < NUM_MAX7219; loop++)
-	{
+	for(int loop = 0;loop < NUM_MAX7219; loop++){
   	HAL_SPI_Transmit(hspi, (uint8_t *)p_data, 1, 100);
 	}
 	HAL_GPIO_WritePin(CS_PORT, CS_PIN, GPIO_PIN_SET);
@@ -23,31 +21,28 @@ void send_all(SPI_HandleTypeDef *hspi, uint16_t data)
 
 void set_row(SPI_HandleTypeDef *hspi, uint8_t row, uint16_t data)
 {
-	switch(row)
-	{
-		case 1: case 2: case 3: case 4: case 5: case 6: case 7:	case 8:
-		{
-      uint8_t temp = 0x00;
+	switch(row){
+		case 1: case 2: case 3: case 4: case 5: case 6: case 7:	case 8:{
+    			uint8_t temp = 0x00;
 			for(int loop = 0; loop<7; loop++){
 				temp |= ((data >> (6-loop) & 0x0001) << loop);
 			}
 			temp &= 0x7F;
 			temp |= (data & 0x0080);
-		  uint16_t packet[NUM_MAX7219] = {0x0000, row << 8 | (data & 0x0400)>>6 |
+		  	uint16_t packet[NUM_MAX7219] = {0x0000, row << 8 | (data & 0x0400)>>6 |
 			(data & 0x0200)>>4 | (data & 0x0100)>>2, 0x0000, row << 8 | temp};
 			send_packet(hspi, (uint8_t *)packet);
 		}	
-	  default:
-    {
-      uint16_t packet[NUM_MAX7219] = {(row - 6) << 8 | (data & 0x0400)>>6 |
+	  	default:{
+      			uint16_t packet[NUM_MAX7219] = {(row - 6) << 8 | (data & 0x0400)>>6 |
 			(data & 0x0200)>>4 | (data & 0x0100)>>2, 0x0000, 0x0000, 0x0000};
-    }
+    		}
 	}	
 }
 
 void led_init(SPI_HandleTypeDef *hspi)
 {
-  send_all(hspi, REG_DECODE_MODE|0x00);
+  	send_all(hspi, REG_DECODE_MODE|0x00);
 	send_all(hspi, REG_INTENSITY|INTENSITY);
 	send_all(hspi, REG_SCANLIMIT|0x07);
 	send_all(hspi, REG_SHUTDOWN|0x01);
@@ -86,47 +81,46 @@ void led_run(SPI_HandleTypeDef *hspi, uint8_t hour, uint8_t minute)
 	update_minute = minute/5;
 	if(update_minute > 6) set_row(hspi, 4, TO);
 	else if(update_minute > 0)  set_row(hspi, 5, PAST);
-	switch(update_minute)
-	  {
-		  case 0:{
+	switch(update_minute){
+		case 0:{
 			 if(hour == 10 || hour == 22) set_row(hspi, 10, TEN|OCLOCK);
-       else	set_row(hspi, 10, OCLOCK);		
+     			 else set_row(hspi, 10, OCLOCK);		
 			 set_row(hspi, 3, 0);
 			 set_row(hspi, 4, 0);
 		  }
-		  case 1: {
+		case 1:{
 			 set_row(hspi, 3, M_FIVE);
 			 if(hour == 7 || hour == 19) set_row(hspi, 5, PAST|SEVEN);
-       else	set_row(hspi, 5, PAST);	
-       if(hour == 10 || hour == 22) set_row(hspi, 10, TEN);
-       else	set_row(hspi, 10, 0);				
+       			 else set_row(hspi, 5, PAST);	
+      			 if(hour == 10 || hour == 22) set_row(hspi, 10, TEN);
+       			 else set_row(hspi, 10, 0);				
 			 break;
 		  }
-		  case 2: {
+		case 2: {
 			 set_row(hspi, 4, M_TEN);
 			 set_row(hspi, 3, 0);
 			 break;
 		  }
-		  case 3: {
+		case 3: {
 			 set_row(hspi, 2, QUARTER);
 			 set_row(hspi, 4, 0);
 			 break;
 		  }
-		  case 4: {
+		case 4: {
 			 set_row(hspi, 3, TWENTY);
 			 set_row(hspi, 2, 0);
 			 break;
 		  }
-		  case 5: {
+		case 5: {
 			 set_row(hspi, 3, TWENTY_FIVE);
 			 break;
 		  }
-		  case 6:{			 
+		case 6:{			 
 			 set_row(hspi, 4, HALF);
 			 set_row(hspi, 3, 0);
 			 break;
 		  }
-			case 7:{			 
+		case 7:{			 
 			 set_row(hspi, 3, TWENTY_FIVE);
 			 set_row(hspi, 4, TO);
 			 hour++;
